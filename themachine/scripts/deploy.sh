cc6a668811d2f635540403e7886f8c8f3d3a3962
João Neto <jneto@MacJN2.lan>
2020-09-22 13:50:41 +0100

    update

--- themachine/scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-# RUN: /Users/jneto/dev/proj/websites/themachine/deploy.sh
-
-PROJ=themachine
-SITE=themachine.global
-cd /Users/jneto/dev/proj/websites/$PROJ
-npm run build
-docker-machine ssh webserver rm -rf /home/ubuntu/websites/$SITE
-docker-machine scp -r build webserver:/home/ubuntu/websites/$SITE
